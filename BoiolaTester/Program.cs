54c99ca15dea65fd48e3e1db920b4c077c013a10
Carlos Ferreira <carlos.m.ferreira@celfinet.com>
2018-07-25 10:55:14 +0100

    Add project files.

---
+++ BoiolaTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoiolaTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
