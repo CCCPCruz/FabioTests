4ca4b65ede32ea79d66b4728cb3000823a8417f5
Carlos Ferreira <carlos.m.ferreira@celfinet.com>
2018-07-26 14:00:28 +0100

    initial Commit

---
+++ BoiolaTester/BoiolaTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoiolaTester
+{
+    [TestFixture]
+    public class FabioTests
+    {
+        Fabio fabio = new Fabio();
+
+        [Test]
+        public void NomeCompletodoFabio()
+        {
+            Assert.AreEqual("Fabio Carvalho", fabio.getFullName());
+        }
+
+        [Test]
+        public void FabioIsRabeta()
+        {
+            Assert.IsTrue(fabio.isBoiola);
+        }
+
+
+        [Test]
+        public void FabioVemdeCanidelo()
+        {
+            Assert.AreEqual("Canidelo, VC", fabio.Location);            
+        }
+
+        [Test]
+        public void FabioNaoGostadeGajas()
+        {
+
+            var fabio = new Fabio();     
+       
+               Assert.IsTrue(!fabio.gostadegajas());
+        }
+        [Test]
+        public void SeraqueOFabioePaneleiro()
+        {
+            Assert.IsTrue(fabio.isBoiola);
+        }
+
+    }
+}
---
+++ BoiolaTester/Fabio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoiolaTester
+{
+    public class Fabio
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Location { get; set; }
+
+        public bool isBoiola { get; set; }
+
+        public Fabio()
+        {
+
+            this.FirstName = "Fabio";
+            this.LastName = "Carvalho";
+            this.Location = "Canidelo, VC";
+            this.isBoiola = true;
+        }
+
+        public bool gostadegajas()
+        {
+
+            return !isBoiola;
+        }
+
+        public string getFullName() {
+            return $"{FirstName} {LastName}";
+        }
+
+    }
+}
